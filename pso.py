04078d1c9878690eb855aa6265a35f410e0231df
Matthew <mathonich@yahoo.com>
2023-05-29 17:51:53 -0700

    Further adding stuff

--- pso.py
+++ pso.py
@@ -5,7 +5,7 @@ class Particle():
     """Represents a particle"""
     dimensions = 2
     # Inertial weight
-    weight = 0.8 
+    weight = 1 
     # Social and cognitive coefficients.
     # Cognitive makes a particle care more about its own findings
     # Social makes a particle care more about the swarm's findings
@@ -18,14 +18,7 @@ class Particle():
     gbest_pos = [upper_bound] * dimensions
     # Target minimum values
     target = [0,0]
-
-    @classmethod
-    def within_target_error(cls, error) -> bool:
-        """Returns true if gbest_pos is within the designated target error"""
-        for d in range(cls.dimensions):
-            if cls.gbest_pos[d] > cls.target[d] + error:
-                return False
-        return True
+    error = 1e-6
 
     @classmethod
     def decrement_weight(cls):
@@ -34,8 +27,9 @@ class Particle():
             cls.weight = cls.weight - 0.01
 
     @classmethod
-    def setup(cls, social:float=1.5, cognitive:float=1.5, dimensions:int=2, upper:float=5.0, lower:float=-5.0, target:list=[0,0]) -> None:
+    def setup(cls, weight:float=1.0, social:float=1.5, cognitive:float=1.5, dimensions:int=2, upper:float=5.0, lower:float=-5.0, target:list=[0,0], error:float=1e-6) -> None:
         """Setup the swarm space"""
+        cls.weight = weight
         cls.social_coefficient = social
         cls.cognitive_coefficient = cognitive
         cls.dimensions = dimensions
@@ -43,16 +37,26 @@ class Particle():
         cls.upper_bound = upper
         cls.lower_bound = lower
         cls.target = target
+        cls.error = error
         assert len(cls.gbest_pos) == cls.dimensions
+
+    @classmethod
+    def within_target_error(cls) -> bool:
+        """Returns true if gbest_pos is within the designated target error"""
+        for d in range(cls.dimensions):
+            if cls.gbest_pos[d] < cls.target[d] + cls.error:
+                if cls.gbest_pos[d] > cls.target[d] - cls.error:
+                    return True 
+        return False 
 
     @classmethod
     def fitness(cls, position):
         """Ackley"""
         x = position[0]
         y = position[1]
-        #return x**2 + y**2
-        return -20.0 * np.exp(-0.2 * np.sqrt(0.5 * (x**2 + y**2))) - np.exp(0.5 * (np.cos(2 * 
-          np.pi * x)+np.cos(2 * np.pi * y))) + np.e + 20
+        return x**2 + y**2
+        # return -20.0 * np.exp(-0.2 * np.sqrt(0.5 * (x**2 + y**2))) - np.exp(0.5 * (np.cos(2 * 
+        #   np.pi * x)+np.cos(2 * np.pi * y))) + np.e + 20
 
     def __init__(self) -> None:
         # Set position. Makes vector with dimensions Particle.dimensions
@@ -78,10 +82,12 @@ class Particle():
             return True
         return False
 
-    def update_gbest_pos(self) -> None:
+    def update_gbest_pos(self) -> bool:
         """Updates gbest based on particle index"""
         if self.fitness(self.pbest_pos) < self.fitness(Particle.gbest_pos):
             Particle.gbest_pos = self.pbest_pos 
+            return True
+        return False
 
     def update_velocity(self) -> None: 
         """Update the particle's velocity in each dimension"""
@@ -93,12 +99,12 @@ class Particle():
             # Find distance to global best pos
             dist_gbest = Particle.gbest_pos[d] - self.position[d]
             # Set cognitive constant
-            p_const = Particle.cognitive_coefficient * np.random.uniform(low=0, high=1)
+            p_const = Particle.cognitive_coefficient * np.random.uniform(low=0.001, high=1)
             # Set the social constant
-            g_const = Particle.social_coefficient * np.random.uniform(low=0, high=1)
+            g_const = Particle.social_coefficient * np.random.uniform(low=0.001, high=1)
             # Set velocity in given dimension
             final_velocity = inertial_velocity + p_const * dist_pbest + g_const * dist_gbest 
-            self.velocity[d] = final_velocity
+            self.velocity[d] = np.float64(final_velocity)
 
     def enforce_bounds(self) -> None:
         """When the position is outside of bounds, it is set in bounds. When a velocity is outside, it is set to 0"""
@@ -110,30 +116,34 @@ class Particle():
                 self.position[d] = Particle.lower_bound
         for d in range(len(self.velocity)):
             if self.velocity[d] > Particle.upper_bound:
-                self.velocity[d] = 0
+                self.velocity[d] = 0 
                 continue
             if self.velocity[d] < Particle.lower_bound:
-                self.velocity[d] = 0
+                self.velocity[d] = 0 
 
-    def search(self):
+    def search(self) -> bool:
         """A single particle's search for minimum"""
         self.update_velocity()
         self.update_position()
         self.enforce_bounds()
         if self.update_pbest_pos():
-            self.update_gbest_pos()
+            if self.update_gbest_pos():
+                return Particle.within_target_error()
+        return False
 
 def particle_swarm_optimization():
-    Particle.setup(social=1.5, cognitive=1.5, dimensions=2, upper=5.0, lower=-5.0, target=[0,0])
+    Particle.setup(social=1.5, cognitive=1.5, dimensions=2, upper=1.0, lower=-1.0, target=[0,0], error=1e-6)
     num_particles = 500
     particles = [Particle()] * num_particles
-    i = 500
-    while i > 0:
+    found_target = False
+    i = 0  
+    while found_target == False:
         for particle in particles:
-            particle.search()
-        Particle.decrement_weight()
-        i -= 1
-    print(f"Gbestpos is: {Particle.gbest_pos}")
+            found_target = particle.search()
+            print(particle)
+        #Particle.decrement_weight()
+        i += 1
+    print(f"Gbestpos is: {Particle.gbest_pos}, in {i} iterations")
 
 
 
